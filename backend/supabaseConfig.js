54c5db89fef4b60610ae7c10017e22669d465826
yairhdz24 <yairhdz107@gmail.com>
2024-04-01 02:05:22 -0600

    Update Supabase configuration

--- backend/supabaseConfig.js
+++ backend/supabaseConfig.js
@@ -1,10 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Lee las variables de entorno desde el archivo .env
-const SUPABASE_URL = ".";
-// https://cfdrkjqgibsxervlmzpr.supabase.co
-const SUPABASE_KEY = ".";
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmZHJranFnaWJzeGVydmxtenByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE3ODM0NTksImV4cCI6MjAyNzM1OTQ1OX0.0VOI328Aqwi3x_-9LRWLxPla3QOm9Cfme9htvJQ3YWc
+const SUPABASE_URL = "https://cfdrkjqgibsxervlmzpr.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmZHJranFnaWJzeGVydmxtenByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE3ODM0NTksImV4cCI6MjAyNzM1OTQ1OX0.0VOI328Aqwi3x_-9LRWLxPla3QOm9Cfme9htvJQ3YWc";
 
 // Configura Supabase con las credenciales
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
