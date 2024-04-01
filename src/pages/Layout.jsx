c38ba7709092cec70960e4c4f4d07164e184db1f
yairhdz24 <yairhdz107@gmail.com>
2024-04-01 01:20:39 -0600

    cambios en las rutas

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 
-const whiteList = ["/protected"];
-
-function Layout() {
+const whiteList = ["/HomePage"]; // Rutas que no requieren autenticacion
+ 
+export const Layout = () => {
   const location = useLocation();
-  const { account } = JSON.parse(localStorage.getItem("faceAuth")) || {};
-
-  if (!account && whiteList.includes(location.pathname)) {
+  const { account } = JSON.parse(localStorage.getItem("faceAuth")) || {}; // Obtener la cuenta del usuario
+ 
+  if (!account && whiteList.includes(location.pathname)) {  
     return <Navigate to="/" />;
   }
 
-  if (account && !whiteList.includes(location.pathname)) {
-    return <Navigate to="/protected" />;
+  if (account && !whiteList.includes(location.pathname)) { 
+    return <Navigate to="/HomePage" />;
   }
 
   return (
-    <div className="h-90 flex flex-col justify-between">
+    <div className="h-90 flex flex-col justify-between"> 
       <Outlet className="grow" />
       {/* <Footer /> */}
     </div>
   );
 }
 
-export default Layout;
+
